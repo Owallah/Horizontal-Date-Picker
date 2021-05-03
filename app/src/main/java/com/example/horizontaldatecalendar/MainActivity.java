9e5eb3fc53ee7756c36abb6e9ff811b36bb52b8c
Owallah <51329456+Owallah@users.noreply.github.com>
2021-05-03 16:11:51 +0300

    Added a circular menu at long press

--- app/src/main/java/com/example/horizontaldatecalendar/MainActivity.java
+++ app/src/main/java/com/example/horizontaldatecalendar/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.horizontaldatecalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
 
 import java.util.Calendar;
 
@@ -13,14 +19,16 @@ import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView selected_date;
+    public CircleMenu circleMenu;
+    public ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        selected_date = findViewById(R.id.selected_date);
+        circleMenu = findViewById(R.id.circle_menu);
+        constraintLayout = findViewById(R.id.my_layout);
 
         /* starts before 1 month from now */
         Calendar startDate = Calendar.getInstance();
@@ -38,11 +46,51 @@ public class MainActivity extends AppCompatActivity {
         horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
             @Override
             public void onDateSelected(Calendar date, int position) {
-                //do something
-                //Toast.makeText(MainActivity.this, "Selected date" + date, Toast.LENGTH_SHORT).show();
-                selected_date.setText("Selected date: " + date);
+
+                circleMenu.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public boolean onDateLongClicked(Calendar date, int position) {
+                circleMenu.setVisibility(View.VISIBLE);
+                return true;
             }
         });
+
+        circleMenu.setMainMenu(Color.parseColor("#940599"),R.drawable.ic_menu,R.drawable.ic_baseline_cancel_24)
+                .addSubMenu(Color.parseColor("#ff8a5c"),R.drawable.ic_task)
+                .addSubMenu(Color.parseColor("#CDCDCD"),R.drawable.ic_schedule)
+                .addSubMenu(Color.parseColor("#88bef5"),R.drawable.ic_notes)
+                .addSubMenu(Color.parseColor("#83e85a"),R.drawable.ic_reminder)
+                .addSubMenu(Color.parseColor("#FF4832"),R.drawable.ic_event)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                    @Override
+                    public void onMenuSelected(int index) {
+                        switch (index){
+                            case 0:
+                                Toast.makeText(MainActivity.this, "task", Toast.LENGTH_SHORT).show();
+                                constraintLayout.setBackgroundColor(Color.parseColor("#ecfffb"));
+                                break;
+                            case 1:
+                                Toast.makeText(MainActivity.this, "schedule", Toast.LENGTH_SHORT).show();
+                                constraintLayout.setBackgroundColor(Color.parseColor("#96f7d2"));
+                                break;
+                            case 2:
+                                Toast.makeText(MainActivity.this, "notes", Toast.LENGTH_SHORT).show();
+                                constraintLayout.setBackgroundColor(Color.parseColor("#fac4a2"));
+                                break;
+                            case 3:
+                                Toast.makeText(MainActivity.this, "reminder", Toast.LENGTH_SHORT).show();
+                                constraintLayout.setBackgroundColor(Color.parseColor("#d3cde6"));
+                                break;
+                            case 4:
+                                Toast.makeText(MainActivity.this, "event", Toast.LENGTH_SHORT).show();
+                                constraintLayout.setBackgroundColor(Color.parseColor("#fff591"));
+                                break;
+                        }
+
+                    }
+                });
 
     }
 }
